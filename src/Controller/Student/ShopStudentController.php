bf2b0e76cec82cb3617a21f971ed57db27748f68
fedy <fedi.larbi@esprit.tn>
2026-02-09 05:23:56 +0100

    cest termine

--- src/Controller/Student/ShopStudentController.php
+++ src/Controller/Student/ShopStudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Student;
 
+use App\Repository\ProductRepository; // Importation nécessaire
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class ShopStudentController extends AbstractController
 {
     #[Route('/', name: 'student_shop_index')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
-        $products = [
-            ['id' => 1, 'name' => 'Livre de Mathématiques', 'price' => 25.00, 'image' => 'book.jpg'],
-            ['id' => 2, 'name' => 'Calculatrice Scientifique', 'price' => 15.00, 'image' => 'calc.jpg'],
-            ['id' => 3, 'name' => 'Cahier de notes EduSmart', 'price' => 5.00, 'image' => 'notebook.jpg'],
-        ];
+        // On récupère les vrais produits depuis la base de données
+        $products = $productRepository->findAll();
 
         return $this->render('student/shop/index.html.twig', [
             'products' => $products,
